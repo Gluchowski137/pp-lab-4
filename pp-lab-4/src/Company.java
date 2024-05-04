d971719b18dc07520fb76c582141a2a3f5ca7ecf
Krzysztof Głuchowski <krzysztofgluchowski2001@gmail.com>
2024-05-04 14:32:14 +0200

    fix zad1

--- pp-lab-4/src/Company.java
+++ pp-lab-4/src/Company.java
@@ -2,8 +2,7 @@ public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
         for (int i = 0; i < employees.length; i++) {
-            employees[i] = new Employee();
-            employees[i].setSalary(3000 + i * 500);
+            employees[i] = new Employee("Employee" + i,3000 + i * 500);
         }
         System.out.println("Dane dla pracownika o indeksierównym 3: "+ employees[3].toString());
         employees[3].setSalary(2000);
